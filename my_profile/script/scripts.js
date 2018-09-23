295c67cee08f2212ee3ffcdd796391601ed624bb
Santosh <santosh.poudyal@gmail.com>
2018-09-23 15:06:24 +1000

    added diamonds

--- my_profile/script/scripts.js
+++ my_profile/script/scripts.js
@@ -1,23 +1,4 @@
-// function openCity(evt, cityName) {
-//     // Declare all variables
-//     var i, tabcontent, tablinks;
 
-//     // Get all elements with class="tabcontent" and hide them
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = "none";
-//     }
-
-//     // Get all elements with class="tablinks" and remove the class "active"
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     }
-
-//     // Show the current tab, and add an "active" class to the button that opened the tab
-//     document.getElementById(cityName).style.display = "block";
-//     evt.currentTarget.className += " active";
-// }
 
 var displayEducation = document.getElementById("link-education");
 var displayWork = document.getElementById("link-work")
@@ -34,9 +15,16 @@ displayWork.onclick = function(){
     workTable[0].style.display="block";
 }
 
-var typed = new Typed('#typed-strings', {
-	strings: ['I am Santosh.', 'I am a: <strong>Web Developer</strong>', 'I am a: <strong>Dad</strong>'],
-	typeSpeed: 100,
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 100,
     backDelay: 3000
+});
 
+$(".diamond-grid").diamonds({
+    size : 200, // Size of diamonds in pixels. Both width and height. 
+    gap : 5, // Pixels between each square.
+    hideIncompleteRow : false, // Hide last row if there are not enough items to fill it completely.
+    autoRedraw : true, // Auto redraw diamonds when it detects resizing.
+    itemSelector : ".items" // the css selector to use to select diamonds-items.
 });
